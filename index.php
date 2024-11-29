fea531ae83f081f8b3f4e882e2c4f5828e9effb8
queijinha <geanlucas13572500@gmail.com>
2024-11-29 16:13:15 -0300

    gostei bastante nota 10

---
+++ index.php
@@ -0,0 +1,29 @@
+<h2>reseita
+    <?php
+    echo ":pao com ovo"
+    ?>
+</h2>
+<table>
+    <thead>
+        <tr>
+            <th>ingredientes</th>
+            <th>quantidade</th>
+        </tr> 
+    <thead>
+    <tbody>
+<tr>
+    <td>pao</td>
+    <td>1</td>
+</tr>
+<tr>
+    <td>ovo</td>
+    <td>3</td>
+</tr>
+</table>
+<h1>nota:</h1>
+<script>
+    var h1=document.querySelector("h1");
+    h1.innerHTML="nota:10";
+</script>
+
+
